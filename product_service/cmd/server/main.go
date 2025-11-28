8cffec07681d86d838312ff07feb725135584e91
YannVanneth <yannvanneth.27@gmail.com>
2025-11-28 22:55:19 +0700

    [TASK] : #11 Register service to SD (Service Discovery Server)

--- product_service/cmd/server/main.go
+++ product_service/cmd/server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"product_service/internal/config"
+	"product_service/internal/config/eureka_client"
 	"product_service/internal/transport/http"
 )
 
-func main(){
-	
+func main() {
+
+	eurekaclient.EurekaClient()
+
 	cfg := config.LoadConfig()
 
 	app := http.NewRouter()
